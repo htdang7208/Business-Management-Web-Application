d77c488deed581f8fbf264f2e957203213b8e9e9
Dang Huynh Thanh <intern.htdang@tma.com.vn>
2020-02-20 14:47:21 +0700

    update 20.2.2020 v3

---
+++ ISCBackend/ISCManageAPI/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISCManageAPI.Models
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admins { get; set; }
+    }
+}
