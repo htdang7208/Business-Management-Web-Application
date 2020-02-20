684068c710ed5acc983aaf16c39ccdb870275314
Dang Huynh Thanh <intern.htdang@tma.com.vn>
2020-02-20 10:06:31 +0700

    update 20.2.2020

--- 1. Documents/Database/BMWA_SQLQuery.sql
+++ 1. Documents/Database/BMWA_SQLQuery.sql
@@ -1,11 +1,14 @@
 use [ISC_Student_Management]
 go
 
-alter table [dbo].[User] alter column Major nvarchar(50)
+alter table Schedule drop column WeekQuantity
+alter table Schedule add Weeki int
+
+alter table Lecturer drop column Password
+alter table Lecturer add AvatarLink varchar(200) 
 
 alter table Student add IntakeID varchar(20)
-alter table Student add Password varchar(1000)
-alter table Lecturer add Password varchar(1000)
+alter table Intake add WeekAmount int
 
 alter table ScheduleDetail add DayInWeek varchar(15)
 alter table ScheduleDetail add LessionBegin int
@@ -14,7 +17,12 @@ alter table ScheduleDetail add TimeBegin time
 alter table ScheduleDetail add LessionTime int
 
 alter table ScheduleDetail drop column Lession
-alter table ScheduleDetail drop column LessionTime
 
 alter table Student alter column Id varchar(20) not null
+alter table Studying alter column StudentID varchar(20)
 alter table Student add constraint PK_Student primary key (Id)
+
+alter table WorkTrack drop constraint FK_WorkTrack_Student
+
+ALTER TABLE WorkTrack DROP CONSTRAINT FK_WorkTrack_Student
+alter table WorkTrack add constraint FK_WorkTrack_Student foreign key (StudentID) references Student(Id)
